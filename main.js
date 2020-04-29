647034811814b554c31b9f74fce9eb61de3cbb90
Tom Anderson <tom@ando.gq>
2020-04-29 21:01:11 +1000

    Add simple edit window

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-// Constants
-const objectCount = 50;
-const maxWeight = 100;
-const gravitationalStrength = 100;
-const maxForce = 10;
-const centerWeight = 1000;
-const tailLength = 100;
+// Parameters
+const parameters = {
+    "Object Count": 50,
+    "Max Weight": 100,
+    "Gravitational Strength": 100,
+    "Max Force": 10,
+    "Center Weight": 1000,
+    "Tail Length": 100
+}
 
 // Globals
 let canvas;
@@ -24,7 +26,7 @@ function update(ctx) {
                 let dist = dir.magnitude;
                 let m1 = object.mass;
                 let m2 = o.mass;
-                dir.normalise().multiply(gravitationalStrength * m1 * m2 / Math.pow(dist, 2));
+                dir.normalise().multiply(parameters["Gravitational Strength"] * m1 * m2 / Math.pow(dist, 2));
                 object.force.add(dir);
             }
         });
@@ -32,26 +34,57 @@ function update(ctx) {
         let dir = Vector.subtract(center, object.pos);
         let dist = dir.magnitude;
         let m = object.mass;
-        dir.normalise().multiply(gravitationalStrength * m * centerWeight / Math.pow(dist, 2));
+        dir.normalise().multiply(parameters["Gravitational Strength"] * m * parameters["Center Weight"] / Math.pow(dist, 2));
         object.force.add(dir);
 
         // Restrict magnitude of force
-        if (object.force.magnitude > maxForce) {
-            object.force.normalise().multiply(maxForce);
+        if (object.force.magnitude > parameters["Max Force"]) {
+            object.force.normalise().multiply(parameters["Max Force"]);
         }
         
         object.update(ctx);
     });
 }
 
-function init() {
-    canvas = new Canvas(document.getElementById("game"));
-
-    for (let i = 0; i < objectCount; i++) {
-        objects.push(new Mover(r(canvas.width), r(canvas.height), r(maxWeight), tailLength));
+function start() {
+    objects = [];
+    for (let i = 0; i < parameters["Object Count"]; i++) {
+        objects.push(new Mover(r(canvas.width), r(canvas.height), r(parameters["Max Weight"]), parameters["Tail Length"]));
     }
 
+}
+
+function init() {
+    canvas = new Canvas(document.getElementById("game"));
     canvas.callbacks.push(update);
+
+    let edit = document.getElementById("edit");
+    Object.keys(parameters).forEach((key) => {
+        let row = document.createElement("div");
+
+        let name = document.createElement("h3");
+        name.innerText = key;
+        row.appendChild(name);
+
+        let input = document.createElement("input");
+        input.type = "number";
+        input.value = parameters[key];
+        input.addEventListener("input", (e) => {
+            parameters[key] = Number(e.target.value);
+            start();
+        });
+        row.appendChild(input);
+
+        edit.appendChild(row);
+    });
+
+    document.body.addEventListener("click", (e) => {
+        if (e.target == edit) edit.classList.add("hidden");
+        else edit.classList.remove("hidden");
+
+    });
+
+    start();
 }
 
 init();
